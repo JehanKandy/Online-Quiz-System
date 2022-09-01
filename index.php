280a5d79b6bf602e3c2de3827ef264463c7a1cb5
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-01 21:04:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
 
 <div class="home-body">
     <div class="title">
-        Welcome to Online Quiz Syste 
+        Welcome to Online Quiz System <br> 
     </div>
     <img src="images/quiz1.png" alt="quiz" class="quiz-img">
     <div class="content">
         Take more then 1 Milion Question From the <br> system and also get your result according to quiz
+        <hr>
+    </div>
+    <div class="footer">
+        This system develop for only educational purposes <br>
+        This system doesn't have following function because this system host in free service
     </div>
 </div>
 
