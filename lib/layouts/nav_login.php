341f2ab68716416a4e6361f5f18a41be5854010c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-01 21:19:16 +0530

    Add files via upload

---
+++ lib/layouts/nav_login.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Online Quiz System</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    </ul>
+    <span class="navbar-text">
+        Quiz
+    </span>
+  </div>
+</nav>
