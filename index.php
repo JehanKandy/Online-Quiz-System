99fa4c03b87199c2363345e680b2b3c3cc333df7
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-01 20:43:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <link rel="stylesheet" href="css/style.css">
 <?php include("lib/layouts/header.php"); ?>
 <?php include("lib/layouts/nav.php"); ?>
+
+<div class="home-body">
+    <div class="title">
+        Welcome to Online Quiz System
+    </div>
+</div>
+
+
+<?php include("lib/layouts/footer.php"); ?>
