3500916c467f4217f5567b19855731a3c5cf7d69
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-01 21:30:01 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,4 +1,13 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_login.php"); ?>
 
+<div class="login-content">
+    <div class="title">
+        Login Here
+    </div>
+    <div class="body">
+        
+    </div>
+</div>
+
 <?php include("../layouts/footer.php"); ?>
