2b2f7ad3522997bf8744f5bada5cea0ec4c97666
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-01 20:29:33 +0530

    Add files via upload

---
+++ lib/layouts/footer.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+<div class="footer">
+    <hr>
+    Copyright <i class="far fa-copyright"></i> file Submission Testing System : 2022 All Right Reserved by <a href="https://github.com/JehanKandy">Jehankandy</a>
+</div>
