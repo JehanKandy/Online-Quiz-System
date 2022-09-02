7a5811a56505f1c612e4a1ec5f384950617f1e9f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-02 22:30:57 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -8,40 +8,34 @@
     </div>
     <div class="body">
         <hr>
-        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="loginfoem" onsubmit="return loginvalidate()">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="loginform" onsubmit="return validateform()">
             <table border="0">
                 <tr>
-                    <td colspan="2">Username : </td>
+                    <td>Username : </td>
                 </tr>
                 <tr>
                     <td>
                         <input type="text" name="username" id="username" class="login-input"><span id="usererrormsg"></span>
                     </td>
-                    <td>
-                        <span id="errorlogo"></span>
-                    </td>
                 </tr>
                 <tr>
-                    <td colspan="2">Password : </td>
+                    <td>Password : </td>
                 </tr>
                 <tr>
                     <td>
                         <input type="password" name="password" id="password" class="login-input"><span id="usererrormsg"></span>
                     </td>
-                    <td>
-                        <span id="errorlogo"></span>
-                    </td>
                 </tr>
                 <tr>
-                    <td colspan="2">
+                    <td>
                         <input type="submit" value="Login" name="login" class="login-btn">
                     </td>
                 </tr>
             </table>
-            <br>
+        </form>
+        <br>
             <a href="pass_reset.php">Forget Password ?</a><br>
             Don't have an Account ? <a href="reg.php">Sign Up</a>
-        </form>
     </div>
 </div>
 
