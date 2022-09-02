7562c8a8e1ed7aafafd042f1c1eff1ab65994f29
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-02 22:25:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function loginvalidate() {
     var loginuser = document.forms['loginform']['username'].value
     if (loginuser == "" || loginuser == null) {
-
+        document.getElementById('usererrormsg').style.color = '#ff3f34';
+        document.getElementById('usererrormsg').innerHTML('Username Cannot Be empty');
+        return false;
     }
 }
