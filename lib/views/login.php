4a065e9a59af9025ec6a8d7f59c36fa1726f2173
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-02 21:15:44 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -6,7 +6,15 @@
         <i class="fas fa-user-alt"></i> Login Here
     </div>
     <div class="body">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+            <p>Username : </p>
+            <input type="text" name="username" id="username" class="login-input">
 
+            <p>Password : </p>
+            <input type="password" name="password" id="password" class="login-input">
+
+            <input type="submit" value="Login" name="login" class="login-btn">
+        </form>
     </div>
 </div>
 
