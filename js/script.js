2a2c3c818479689a30e5350b84b63c505c3ee25b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-02 22:35:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function validateform() {
     }
     var pass = document.forms["loginform"]["password"].value
     if (pass == "" || pass == null) {
-        document.getElementById('usererrormsg').style.color = '#ff3f34';
-        document.getElementById('usererrormsg').innerHTML = '<span style="font-size:12px;">Password connot be empty</span>';
-        document.getElementById('username').style.borderColor = '#ff3f34';
+        document.getElementById('passerrormsg').style.color = '#ff3f34';
+        document.getElementById('passerrormsg').innerHTML = '<span style="font-size:12px;">Password connot be empty</span>';
+        document.getElementById('password').style.borderColor = '#ff3f34';
         return false;
     }
 }
