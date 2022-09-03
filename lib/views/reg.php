bf84ade5c3fd5b1d74bd23e9042d5d3c77e9bb84
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-03 11:16:14 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -5,6 +5,12 @@
     <div class="title">
         <i class="fas fa-user-plus"></i> Register Here 
     </div>
+    <div class="body">
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+    
+    
+        </form>
+    </div>
 </div>
 
 <?php include("../layouts/footer.php"); ?>
