66cdf2bcc4fb643e7b10b33fbc2c0071c89298e1
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-03 11:49:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,13 @@ function validateform() {
         return false;
     }
 }
+
+function validateform_reg() {
+    var usern = document.forms["loginform"]["username"].value
+    if (usern == "" || usern == null) {
+        document.getElementById('usererrormsg').style.color = '#ff3f34';
+        document.getElementById('usererrormsg').innerHTML = '<span style="font-size:12px;">Username connot be empty</span>';
+        document.getElementById('username').style.borderColor = '#ff3f34';
+        return false;
+    }
+}
