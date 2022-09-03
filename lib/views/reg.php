fab49fcdecb7c92f35d1fc1dcd8293a6e69e2f20
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-03 11:13:29 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -1,5 +1,10 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_login.php");?>
 
+<div class="reg-content">
+    <div class="title">
+        Register Here 
+    </div>
+</div>
 
 <?php include("../layouts/footer.php"); ?>
