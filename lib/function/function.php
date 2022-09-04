5fee92c8daf6c403182a7884576d72cb367ed303
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-04 20:38:40 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -5,5 +5,13 @@
 
     function reg_user($username,$email,$password){
         $con = Connection();
+
+        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email'";
+        $check_user_result = mysqli_query($con, $check_user);
+        $check_user_nor = mysqli_num_rows($check_user_result);
+        
+        if(){
+            
+        }
     }
 ?>
