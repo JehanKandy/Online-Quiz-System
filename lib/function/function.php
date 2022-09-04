30d7df304eaae2de8b20bf2819a544be7f684ec3
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-04 20:42:06 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -3,17 +3,17 @@
     use FTP\Connection;
     session_start();
 
-    function reg_user($username,$email,$password){
+    function reg_std($username,$email,$password){
         $con = Connection();
 
-        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email'";
+        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email' && roll = 'student'";
         $check_user_result = mysqli_query($con, $check_user);
         $check_user_nor = mysqli_num_rows($check_user_result);
         
         if($check_user_nor > 0){
             return "<span style='color:red;'>User Already Exists</span>";
         }else{
-            
+
         }
 
     }
