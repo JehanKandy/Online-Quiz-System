6bd1bfb673a4f395510b9ea69c696a47078cbaa4
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-04 20:05:12 +0530

    Add files via upload

---
+++ lib/views/reg_teacher.php
@@ -0,0 +1,67 @@
+<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/nav_login.php");?>
+
+<div class="reg-content">
+    <div class="title">
+        <i class="fas fa-user-plus"></i> Register Teacher
+    </div>
+    <div class="body">
+        <hr>
+        <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="regform" onsubmit="return validateform_reg()">
+            <table border="0">
+                <tr>
+                    <td>
+                        Username : 
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="username" id="username" class="reg-input" placeholder="Username"><span id="regusererr"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Email :
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="email" name="email" id="email" class="reg-input" placeholder="Email"><span id="regemailerr"></span>
+                    </td>                
+                <tr>
+                    <td>
+                        Password :
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="password" name="password" id="password" class="reg-input" placeholder="Password"><span id="regpasserr"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Confirm Password :
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="password" name="cpassword" id="cpassword" class="reg-input" placeholder="Confirm Password"><span id="regcpasserr"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="reset" value="Clear" class="reg-clear-btn"> 
+                        <input type="submit" value="Sign Up" name="register" class="reg-btn">
+                    </td>
+                </tr>
+            </table>    
+        </form>
+        <br>
+        Already Have Account ? <a href="login.php">Login</a><br>
+        Teacher Register ? <a href="reg_teacher.php">Teacher</a>
+    </div>
+</div>
+
+<?php include("../layouts/footer.php"); ?>
+
+<script src="../../js/script.js"></script>
