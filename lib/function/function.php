7d1da7fdba1a9ef216eba9364157bd74c73eb054
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-04 21:20:18 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -11,7 +11,23 @@
         $check_user_nor = mysqli_num_rows($check_user_result);
         
         if($check_user_nor > 0){
-            return "<span style='color:red;'>User Already Exists</span>";
+            return "<span style='color:red;'>Teacher Already Exists</span>";
+        }else{
+            $user_insert = "INSERT INTO user_tbl(username,email,pass1,roll,user_status,join_date,is_pending)VALUES('$username','$email','$password','student','1',NOW(),'0')";
+            $user_insert_result = mysqli_query($con,$user_insert);
+            header("location:../views/login.php");
+        }
+    }
+
+    function reg_teacher($username,$email,$password){
+        $con = Connection();
+
+        $check_user = "SELECT * FROM user_tbl WHERE username = '$username' && email = '$email' && roll = 'teacher'";
+        $check_user_result = mysqli_query($con, $check_user);
+        $check_user_nor = mysqli_num_rows($check_user_result);
+        
+        if($check_user_nor > 0){
+            return "<span style='color:red;'>Teacher Already Exists</span>";
         }else{
             $user_insert = "INSERT INTO user_tbl(username,email,pass1,roll,user_status,join_date,is_pending)VALUES('$username','$email','$password','student','1',NOW(),'0')";
             $user_insert_result = mysqli_query($con,$user_insert);
