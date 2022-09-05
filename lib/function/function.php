4c2ea7c26bece61a85af86de516601f3510599e2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-05 19:04:29 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -45,7 +45,7 @@
     function user_login($username, $password){
         $con = Connection();
     
-        $user_check = "SELECT * FROM user_tbl WHERE username = '$username' && pass1 = '$password' && user_status = '1' && is_pending = '0'";
+        $user_check = "SELECT * FROM user_tbl WHERE username = '$username' && pass1 = '$password' && user_status = '1'";
         $user_check_result = mysqli_query($con, $user_check);
         $user_check_nor = mysqli_num_rows($user_check_result);
         $user_check_row = mysqli_fetch_assoc($user_check_result);
