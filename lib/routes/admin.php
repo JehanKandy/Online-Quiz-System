88b74301b7377c61111d862e7bb460b513c6e386
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-11 19:33:19 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -20,9 +20,8 @@
 			<a href="admin.php" class="menu-item"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
 			<a href="admin/users.php" class="menu-item"><i class="fas fa-user-alt"></i>Students</a>
 			<a href="admin/admins.php" class="menu-item"><i class="fas fa-user-tie"></i>Teachers</a>
-			<a href="admin/staff.php" class="menu-item"><i class="fas fa-user-tag"></i>Staff</a>
-			<a href="admin/vehicles.php" class="menu-item"><i class="fas fa-car-alt"></i>Vehicles</a>
-			<a href="admin/my_infor.php" class="menu-item"><i class="fas fa-chalkboard-teacher"></i>My Details</a>
+			<a href="admin/staff.php" class="menu-item"><i class="fas fa-user-tag"></i>Quizzes</a>
+			<a href="admin/vehicles.php" class="menu-item"><i class="fas fa-car-alt"></i>Admin</a>
 			<a href="admin/my_account_admin.php" class="menu-item"><i class="fas fa-user-cog"></i>Account Settings</a>
 		</nav>
 
