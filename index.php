a58d47b4b9b6177a8c111db623cf1c0a6116aafa
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-14 20:27:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,17 @@
 <?php include("lib/layouts/nav.php"); ?>
 
 <div class="home-img"></div>
-<div class="home-body">
-    <div class="home-grid">
-        <div class="grid-item1">hi1</div>
-        <div class="grid-item2">hi1</div>
-        <div class="grid-item3">hi1</div>
-        <div class="grid-item4">hi1</div>
-        <div class="grid-item5">hi1</div>
-        <div class="grid-item6">hi1</div>
-        <div class="grid-item7">hi1</div>
-        <div class="grid-item8">hi1</div>
-    </div>
+<div class="home-grid">
+    <div class="grid-item1">hi1</div>
+    <div class="grid-item2">hi1</div>
+    <div class="grid-item3">hi1</div>
+    <div class="grid-item4">hi1</div>
+    <div class="grid-item5">hi1</div>
+    <div class="grid-item6">hi1</div>
+    <div class="grid-item7">hi1</div>
+    <div class="grid-item8">hi1</div>
 </div>
+
 
 
 <?php include("lib/layouts/home_footer.php"); ?>
