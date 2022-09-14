12079cc2974496752765536e4e20c81fb37d5f6c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-14 20:26:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,28 +2,19 @@
 <?php include("lib/layouts/header.php"); ?>
 <?php include("lib/layouts/nav.php"); ?>
 
+<div class="home-img"></div>
 <div class="home-body">
-    <div class="title">
-        Welcome to Online Quiz System <br> 
-    </div>
-    <img src="images/quiz1.png" alt="quiz" class="quiz-img">
-    <div class="content">
-        Take more then 1 Million Question From the <br> system and also get your result according to quiz
-        <hr>
-    </div>
-    <div class="footer">
-        This system develop for only educational purposes <br>
-        This system doesn't have following function because this system host in free service
-
-        <br><br>
-        <i class="fas fa-exclamation-circle" style="color: red;"></i> &nbsp; Password Reset function <br>
-        <i class="fas fa-exclamation-circle" style="color: red;"></i> &nbsp; Password and email Verfy function <br>
-
-        <br><br>
-        Download Full Project Here : <a href="https://github.com/JehanKandy/Online-Quiz-System" target="_blank"><i class="fas fa-download"></i>Download</a>
-        
+    <div class="home-grid">
+        <div class="grid-item1">hi1</div>
+        <div class="grid-item2">hi1</div>
+        <div class="grid-item3">hi1</div>
+        <div class="grid-item4">hi1</div>
+        <div class="grid-item5">hi1</div>
+        <div class="grid-item6">hi1</div>
+        <div class="grid-item7">hi1</div>
+        <div class="grid-item8">hi1</div>
     </div>
 </div>
 
 
-<?php include("lib/layouts/footer.php"); ?>
+<?php include("lib/layouts/home_footer.php"); ?>
