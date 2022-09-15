646ad3463e5a46afccb1624c4fc49b5aa7ed0d14
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-15 15:11:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="css/style.css">
-<?php include("lib/layouts/header.php"); ?>
-<?php include("lib/layouts/nav.php"); ?>
+<?php include "lib/layouts/header.php";?>
+<?php include "lib/layouts/nav.php";?>
 
 <div class="home-img"></div>
 <div class="home-title">
     Quizzes are in following Topics
 </div>
 <div class="home-grid">
-    <div class="grid-item1"></div>
+    <div class="grid-item1">Environment</div>
     <div class="grid-item2"></div>
     <div class="grid-item3"></div>
     <div class="grid-item4"></div>
@@ -19,4 +19,4 @@
 
 
 
-<?php include("lib/layouts/home_footer.php"); ?>
+<?php include "lib/layouts/home_footer.php";?>
