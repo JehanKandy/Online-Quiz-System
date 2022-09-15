05f16473a103b1bad05fd37b9f7ecb180f5535d7
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-15 17:17:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="grid-item5">In Sports</div>
     <div class="grid-item6">In Pollution </div>
     <div class="grid-item7">In IQ</div>
-    <div class="grid-item8">IT</div>
+    <div class="grid-item8">In IT</div>
 </div>
 
 
