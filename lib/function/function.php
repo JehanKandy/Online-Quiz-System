2f96a4b75a3cb01089c20122d31c9f7646cd5151
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-21 17:20:48 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -111,4 +111,9 @@
             <img src='../../../upload/".$check_user_img_row['profile_img']."' alt='Profile Image' class='profile-img'>
         ";
     }
+    function std_data(){
+        $con = Connection();
+
+        
+    }
 ?>
