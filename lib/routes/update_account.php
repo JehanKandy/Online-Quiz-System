117d859a639397920a2d3d073bff2ce803069aeb
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-22 19:00:01 +0530

    Update update_account.php

--- lib/routes/update_account.php
+++ lib/routes/update_account.php
@@ -1,6 +1,12 @@
+<link rel="stylesheet" href="../../css/dashboard.css">
 <?php include("../layouts/header.php"); ?>
 <?php include("../function/function.php"); ?>
 
 <?php account_update(); ?>
+
+<?php 
+    if(isset($_POST['']))
+
+?>
 
 <?php include("../layouts/footer.php"); ?>
