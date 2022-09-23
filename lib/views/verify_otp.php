399c1072bdd55c55a27ec5fe7f39b284b5560ba2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 04:36:37 +0530

    Update verify_otp.php

--- lib/views/verify_otp.php
+++ lib/views/verify_otp.php
@@ -2,4 +2,10 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_login.php"); ?>
 
+<div class="verify-otp">
+    <div class="title">
+        Verify OTP
+    </div>
+</div>
+
 <?php include("../layouts/footer.php"); ?>
