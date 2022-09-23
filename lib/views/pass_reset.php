73e35108e3d33b891932dfab2efeb5e4f346f4dd
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-23 19:15:50 +0530

    Update pass_reset.php

--- lib/views/pass_reset.php
+++ lib/views/pass_reset.php
@@ -3,4 +3,10 @@
 <?php include("../layouts/nav_login.php"); ?>
 
 
+<div class="pass-reset-email">
+    <form action="<?php echo($SERVER['PHP_SELF'])?>" method="POST">
+
+    </form>
+</div>
+
 <?php include("../layouts/footer.php"); ?>
