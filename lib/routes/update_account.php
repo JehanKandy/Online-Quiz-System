e7b057786121543ee668726355477afc15cee039
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-23 07:29:00 +0530

    Update update_account.php

--- lib/routes/update_account.php
+++ lib/routes/update_account.php
@@ -2,14 +2,15 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../function/function.php"); ?>
 
-<?php account_update(); ?>
-
 <?php 
     if(isset($_POST['update_user_data'])){
         $result = update_data_user($_FILES['image']['name'], $_POST['user_id'], $_POST['update_username'], $_POST['update_fn'], $_POST['update_ln'], $_POST['update_mobile'], $_POST['update_address'], $_POST['update_city'], $_POST['update_country']);
         echo $result;
     }
-
 ?>
+
+
+<?php account_update(); ?>
+
 
 <?php include("../layouts/footer.php"); ?>
