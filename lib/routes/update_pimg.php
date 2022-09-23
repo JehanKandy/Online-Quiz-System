3978643d78dae8963e924ec16e24ea3abfb02a28
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-23 10:49:33 +0530

    Update update_pimg.php

--- lib/routes/update_pimg.php
+++ lib/routes/update_pimg.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include("../layouts/header.php"); ?>
+<?php 
+    include("../function/function.php");
+
+    if(empty($_SESSION['LoginSession'])){
+        header("../views/login.php");
+    }
+?>
+
 
 <?php include("../layouts/footer.php"); ?>
