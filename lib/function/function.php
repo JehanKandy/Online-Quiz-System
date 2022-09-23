0f5eb3a47d0add7dc9c402989ebd39e0ee0b6291
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-23 11:16:41 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -11,7 +11,7 @@
         $check_user_nor = mysqli_num_rows($check_user_result);
         
         if($check_user_nor > 0){
-            return "<span style='color:red;'>Teacher Already Exists</span>";
+            return "<span style='color:red;'>Student Already Exists</span>";
         }else{
             $user_insert = "INSERT INTO user_tbl(username,email,pass1,roll,user_status,join_date,is_pending)VALUES('$username','$email','$password','student','1',NOW(),'0')";
             $user_insert_result = mysqli_query($con,$user_insert);
