162bb4a480fccfb41463fbde59eae1640ed32579
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 16:53:22 +0530

    Update pass_edit.php

--- lib/views/pass_edit.php
+++ lib/views/pass_edit.php
@@ -4,8 +4,14 @@
 
 <div class="pass-new-edit">
     <div class="title">
-        
+        Change Your Password
     </div>
+    <form action="<?php echo($_SERVER['PHP_SELF'])?>" method="POST">
+        <span class="form-topic">Email Address : </span><br>
+        <input type="email" name="email" id="passnewemail" class="new-pass-input">
+        
+
+    </form>
 </div>
 
 <?php include("../layouts/footer.php"); ?>
