7c9390e1c08b2912c5e4195986ec018da12fa21e
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 17:26:04 +0530

    Update pass_edit.php

--- lib/views/pass_edit.php
+++ lib/views/pass_edit.php
@@ -10,15 +10,15 @@
         <form action="<?php echo($_SERVER['PHP_SELF'])?>" method="POST" name="new_pass_edit" onsubmit="return passnew()">
             <span class="form-topic">Email Address : </span><br>
             <input type="email" name="email" id="passnewemail" class="new-pass-input" placeholder="Email"><br>
-            <span id="emailerror"></span>
+            <span id="emailerror"></span><br>
             
             <span class="form-topic">New Password : </span><br>
             <input type="password" name="new_pass" id="newpass" class="new-pass-input" placeholder="New Password"><br>
-            <span id="npasserror"></span>
+            <span id="npasserror"></span><br>
 
             <span class="form-topic">Confirm New Password : </span><br>
             <input type="password" name="new_cpass" id="newcpass" class="new-pass-input" placeholder="Confirm New Password"><br>
-            <span id="cnpasserror"></span>
+            <span id="cnpasserror"></span><br>
 
             <input type="submit" value="Reset Password" name="reset_new_pass" class="new-pass-btn">
         </form>
