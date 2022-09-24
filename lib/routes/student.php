74b076854f1568e09569b8e1cf313119bcf3ca8f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 21:12:50 +0530

    Update student.php

--- lib/routes/student.php
+++ lib/routes/student.php
@@ -3,6 +3,15 @@
 <?php include("../layouts/nav_loged.php"); ?>
 
 <div class="container">
-    hi
+    <div class="student-panal">
+        <div class="student-grid">
+            <div class="student-item1">1</div>
+            <div class="student-item2">2</div>
+            <div class="student-item3">3</div>
+            <div class="student-item4">4</div>
+            <div class="student-item5">5</div>
+            <div class="student-item6">6</div>
+        </div>
+    </div>
 </div>
 <?php include("../layouts/footer.php"); ?>
