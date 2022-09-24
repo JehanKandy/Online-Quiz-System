5013c5eba229dc09338e3672452c95595ff71cff
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 16:50:20 +0530

    Update pass_edit.php

--- lib/views/pass_edit.php
+++ lib/views/pass_edit.php
@@ -2,4 +2,10 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_login.php"); ?>
 
+<div class="pass-new-edit">
+    <div class="title">
+        
+    </div>
+</div>
+
 <?php include("../layouts/footer.php"); ?>
