f08861050755ba075c64a1485a228fad1c3b5dc0
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-24 17:00:54 +0530

    Update pass_edit.php

--- lib/views/pass_edit.php
+++ lib/views/pass_edit.php
@@ -2,25 +2,27 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_login.php"); ?>
 
-<div class="pass-new-edit">
-    <div class="title">
-        Change Your Password
+<div class="container">
+    <div class="pass-new-edit">
+        <div class="title">
+            Change Your Password
+        </div>
+        <form action="<?php echo($_SERVER['PHP_SELF'])?>" method="POST">
+            <span class="form-topic">Email Address : </span><br>
+            <input type="email" name="email" id="passnewemail" class="new-pass-input" placeholder="Email"><br>
+            <span id="emailerror"></span>
+            
+            <span class="form-topic">New Password : </span><br>
+            <input type="password" name="new_pass" id="newpass" class="new-pass-input" placeholder="New Password"><br>
+            <span id="npasserror"></span>
+
+            <span class="form-topic">Confirm New Password : </span><br>
+            <input type="password" name="new_cpass" id="newcpass" class="new-pass-input"><br>
+            <span id="cnpasserror"></span>
+
+            <input type="submit" value="Reset Password" name="reset_new_pass" class="new-pass-btn">
+        </form>
     </div>
-    <form action="<?php echo($_SERVER['PHP_SELF'])?>" method="POST">
-        <span class="form-topic">Email Address : </span><br>
-        <input type="email" name="email" id="passnewemail" class="new-pass-input" placeholder="Email"><br>
-        <span id="emailerror"></span>
-        
-        <span class="form-topic">New Password : </span><br>
-        <input type="password" name="new_pass" id="newpass" class="new-pass-input" placeholder="New Password"><br>
-        <span id="npasserror"></span>
-
-        <span class="form-topic">Confirm New Password : </span><br>
-        <input type="password" name="new_cpass" id="newcpass" class="new-pass-input"><br>
-        <span id="cnpasserror"></span>
-
-        <input type="submit" value="Reset Password" name="reset_new_pass" class="new-pass-btn">
-    </form>
 </div>
 
 <?php include("../layouts/footer.php"); ?>
