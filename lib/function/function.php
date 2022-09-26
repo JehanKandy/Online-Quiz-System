34e441a9f354b5f1ce678081a858a6a698a90d71
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-26 15:01:00 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -495,11 +495,11 @@
         $std_data_view = "
             <div class='std-data-grid'>
                 <div class='std-data1'>
-                    <span class='std-data-topic'>Username</span><br>
+                    <span class='std-data-topic'>Username : </span><br>
                     <input type='text' class='std-data-input' disabled><br>
                 </div>
                 <div class='std-data2'>
-                    <span class='std-data-topic'>Username</span><br>
+                    <span class='std-data-topic'>First Name : </span><br>
                     <input type='text' class='std-data-input' disabled><br>
                 </div>
             </div>        
