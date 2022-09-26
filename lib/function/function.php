342f92c54f9b460072f6d68a452a3d0627a7246b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-26 15:03:04 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -511,7 +511,7 @@
                     <input type='text' class='std-data-input' disabled><br>
                 </div>  
                 <div class='std-data5'>
-                    <span class='std-data-topic'>Email : </span><br>
+                    <span class='std-data-topic'>Mobile Number : </span><br>
                     <input type='text' class='std-data-input' disabled><br>
                 </div>               
             </div>        
