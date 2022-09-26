bde73e56405c2c082046840366a405765a746b6e
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-26 15:06:43 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -521,7 +521,11 @@
                 <div class='std-data7'>
                     <span class='std-data-topic'>City : </span><br>
                     <input type='text' class='std-data-input' disabled><br>
-                </div>           
+                </div>  
+                <div class='std-data7'>
+                    <span class='std-data-topic'>Country : </span><br>
+                    <input type='text' class='std-data-input' disabled><br>
+                </div>          
             </div>        
         ";
 
