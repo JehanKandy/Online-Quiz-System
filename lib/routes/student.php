6e8ce8f0ee7da6b21b824267f945c54935f35e29
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-27 22:26:50 +0530

    Update student.php

--- lib/routes/student.php
+++ lib/routes/student.php
@@ -109,9 +109,14 @@
                         <div class="std-user">Name : Kamal Perera</div>
                     </td>                    
                 </tr>
+                <tr>
+                    <td>
+                        <div class="std-roll">Roll : Teacher</div>
+                    </td>
+                </tr>
             </table>
 
-			<div class="std-roll">Roll : Teacher</div>
+
 			<div class="std-msg-body">
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis odit dolorem deserunt eveniet, dignissimos dicta eum nobis quaerat, maiores animi reprehenderit aliquid atque, amet itaque possimus est numquam vitae rerum?
 			</div>            
