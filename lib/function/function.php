368b2adb094834eaf5aaebff2136abb942722fd5
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-28 20:19:59 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -645,7 +645,7 @@
                     <td><a href=''><button class='view-std-question'>View</button></td>";
 
                     if($row['is_pending'] == 1){
-                        $std_question .="<td><div class='panding-std-question'></div></td>";                        
+                        $std_question .="<td><div class='panding-std-question'>Pending</div></td>";                        
                     }
 
             $std_question .="
