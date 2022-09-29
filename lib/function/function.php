508bab3ce791d7ab98984d648519df71f72f1d67
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-29 07:44:09 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -673,7 +673,7 @@
                 <span>Question Date : <input type='text' class='question-view_input' value='".$get_question_row['ask_date_time']."' disabled></span>
             
                 <br><br>
-                <textarea class='view-question'>".$get_question_row['question']."</textarea>
+                <textarea class='view-question' disabled>".$get_question_row['question']."</textarea>
             ";
 
             echo $std_view;
