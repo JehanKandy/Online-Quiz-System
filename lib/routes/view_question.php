c6204a0fd4420ea381747d33ce4931a0f300c43e
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-29 07:11:48 +0530

    Update view_question.php

--- lib/routes/view_question.php
+++ lib/routes/view_question.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include("../layouts/header.php"); ?>
-
+<?php include("../function/function.php"); ?>
 
 
 <?php include("../layouts/footer.php"); ?>
