ca02a15626aec4c5e697a0d86d5dfa6593d04d7a
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-29 07:21:25 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -664,6 +664,7 @@
 
         $get_question = "SELECT * FROM question_ask_std_tbl WHERE ask_by_username= '$id'";
         $get_question_result = mysqli_query($con, $get_question);
+        $get_question_row = mysqli_fetch_assoc($get_question_result);
     }
 
 ?>
