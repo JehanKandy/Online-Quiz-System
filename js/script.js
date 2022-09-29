3c7e4c0bcf99b2726bcadf770a29590d34479728
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-29 20:07:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -133,5 +133,7 @@ function questionCheck() {
 
 function answerValidate() {
     var anserer = document.forms['anserForm']['anserreply'].value
-    if ()
+    if (anserer == "" || anserer == null){
+        
+    }
 }
