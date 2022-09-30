e2286542d7d66971d6aaa3f27f09f464de5e89ed
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-09-30 06:45:11 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -723,6 +723,7 @@
         $update_trigger_tbl = "UPDATE question_replyed_std_tbl SET reply_username = '$email', reply_time = NOW(), answer_for_question = '$answer', is_pending = '0' WHERE ask_username = '$answer_by'";
         $update_trigger_tbl_result = mysqli_query($con, $update_trigger_tbl);
 
+        $get_data = "SELECT * FROM question_replyed_std_tbl WHERE ask_username"
 
         $answer_std = "Your Answer for Question: ".$row['question']." and the Answer is: ".$row['answer_for_question'];
 
