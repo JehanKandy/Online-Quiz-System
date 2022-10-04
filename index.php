7f8ad1413ed3823d5ea008bba6403c775fb8ff74
JehanKandy <jehankandy@gmail.com>
2022-10-04 19:48:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/style.css">
 <?php include "lib/layouts/header.php";?>
 <?php include "lib/layouts/nav.php";?>
-
+<?php include "lib/function/function.php"; ?>
 
 <div class="home-img"></div>
 <div class="home-title">
