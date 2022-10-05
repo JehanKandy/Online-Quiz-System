1104c9f3880a9f1988fdd003571d5cb71e847707
JehanKandy <jehankandy@gmail.com>
2022-10-05 20:48:41 +0530

    Update all_commnet.php

--- all_commnet.php
+++ all_commnet.php
@@ -1,12 +1,14 @@
 <?php include "lib/layouts/header.php"; ?>
 <?php include "lib/layouts/nav_login.php"; ?>
 
-<div class="all-comment">
-    <div class="title">
-        All Comments
-    </div>
-    <div class="body">
-        
+<div class="container">
+    <div class="all-comment">
+        <div class="title">
+            All Comments
+        </div>
+        <div class="body">
+            
+        </div>
     </div>
 </div>
 
