c4eb792f52fa91b5d4f97c3f7e92a38d73e3e501
JehanKandy <jehankandy@gmail.com>
2022-10-05 20:39:26 +0530

    Update all_commnet.php

--- all_commnet.php
+++ all_commnet.php
@@ -1,6 +1,15 @@
 <?php include "lib/layouts/header.php"; ?>
 <?php include "lib/layouts/nav_login.php"; ?>
 
+<div class="all-comment">
+    <div class="title">
+        All Comments
+    </div>
+    <div class="body">
+        
+    </div>
+</div>
+
 
 <?php include "lib/layouts/home_footer.php"; ?>
 <script src="js/script.js"></script>
