33cd470f3d0082d098cf5df126f944dc1b88a54a
JehanKandy <jehankandy@gmail.com>
2022-10-06 20:20:49 +0530

    Update comment.php

--- lib/routes/comment.php
+++ lib/routes/comment.php
@@ -1,4 +1,13 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_loged.php"); ?>
 
+<div class="user-comment">
+    <div class="title">
+        Post a Comment
+    </div>
+    <div class="body">
+        Comment body
+    </div>
+</div>
+
 <?php include("../layouts/footer.php"); ?>
