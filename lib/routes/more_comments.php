f931cf57206262dbcba1b389fc7990a12f6fbd35
JehanKandy <jehankandy@gmail.com>
2022-10-07 05:20:36 +0530

    Update more_comments.php

--- lib/routes/more_comments.php
+++ lib/routes/more_comments.php
@@ -1,5 +1,10 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_loged.php"); ?>
+<?php 
+	if(empty($_SESSION['LoginSession'])){
+		header("location:../views/login.php");
+	}
+?>
 
 
 <?php include("../layouts/footer.php"); ?>
