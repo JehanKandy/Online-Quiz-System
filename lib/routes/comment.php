f6b4c50adaa3468bce1c0da39346669c691cffe7
JehanKandy <jehankandy@gmail.com>
2022-10-06 20:30:34 +0530

    Update comment.php

--- lib/routes/comment.php
+++ lib/routes/comment.php
@@ -1,6 +1,11 @@
 <link rel="stylesheet" href="../../css/dashboard.css">
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/nav_loged.php"); ?>
+<?php 
+	if(empty($_SESSION['LoginSession'])){
+		header("location:../views/login.php");
+	}
+?>
 
 <div class="container">
     <div class="user-comment">
