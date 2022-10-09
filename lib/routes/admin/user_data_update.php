0a5160a3a32604e52441cfafe0188613111f5779
JehanKandy <jehankandy@gmail.com>
2022-10-09 20:36:14 +0530

    Update user_data_update.php

--- lib/routes/admin/user_data_update.php
+++ lib/routes/admin/user_data_update.php
@@ -1,4 +1,11 @@
 <?php include("../../layouts/header.php"); ?>
 <?php include("../../layouts/nav_loged_user.php"); ?>
+<?php 
+	if(empty($_SESSION['LoginSession'])){
+		header("location:../../views/login.php");
+	}
+?>
+
+
 
 <?php include("../../layouts/footer.php"); ?>
