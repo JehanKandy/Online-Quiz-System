801236cae92700a44b924c235d9efceddda8dd5a
JehanKandy <jehankandy@gmail.com>
2022-10-09 19:31:41 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -912,6 +912,8 @@
         $count_stu = "SELECT * FROM user_tbl WHERE roll = 'student'";
         $count_stu_result = mysqli_query($con, $count_stu);
         $count_stu_nor = mysqli_num_rows($count_stu_result);
+
+        echo $count_stu_nor;
     }
 
 ?>
