aa6371785e5b497ad4fcaf430b134b97dd41227c
JehanKandy <jehankandy@gmail.com>
2022-10-10 19:09:17 +0530

    Update user_data_update.php

--- lib/routes/admin/user_data_update.php
+++ lib/routes/admin/user_data_update.php
@@ -2,13 +2,21 @@
 <link rel="stylesheet" href="../../../css/style.css">
 <?php include("../../layouts/header.php"); ?>
 <?php include("../../layouts/nav_loged_user.php"); ?>
+
 <?php 
 	if(empty($_SESSION['LoginSession'])){
 		header("location:../../views/login.php");
 	}
 ?>
 
-
+<div class="data-update-user">
+	<div class="title">
+		Update Information
+	</div>
+	<div class="body">
+		hi all
+	</div>
+</div>
 
 
 <?php include("../../layouts/footer.php"); ?>
